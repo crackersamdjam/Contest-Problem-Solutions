9ea61256ea425498ed5f39a4197c494c3c594f05
crackersamdjam <45298690+crackersamdjam@users.noreply.github.com>
2021-07-04 11:42:49 -0400

    Fix indentation

--- CCO/cco20p1.cpp
+++ CCO/cco20p1.cpp
@@ -15,30 +15,30 @@ int main(){
 	cin.tie(0);
 	cin.exceptions(cin.failbit);
 	
-    cin>>n>>l>>r>>y;
-    mp[l] = mp[r+1] = 0;
-    
-    for(int i = 0; i < n; i++){
-        ll x, v, h;
-        cin>>x>>h>>v;
-        
-        ll xx = ceil((ld)v*y/h + x);
-        mp[max(xx, l)]--;
-        
-        xx = floor((ld)v*-y/h + x) + 1;
-        mp[max(xx, l)]++;
-    }
-    ll last = l, cnt = 0;
-    for(auto [i, v]: mp){
-        ll d = i-last;
-        last = i;
-        psa[cnt] += d;
-        cnt += v;
-        if(i == r+1) break;
-    }
-    cout<<psa[0]<<'\n';
-    for(int i = 1; i <= n; i++){
-        psa[i] += psa[i-1];
-        cout<<psa[i]<<'\n';
-    }
+	cin>>n>>l>>r>>y;
+	mp[l] = mp[r+1] = 0;
+	
+	for(int i = 0; i < n; i++){
+		ll x, v, h;
+		cin>>x>>h>>v;
+		
+		ll xx = ceil((ld)v*y/h + x);
+		mp[max(xx, l)]--;
+		
+		xx = floor((ld)v*-y/h + x) + 1;
+		mp[max(xx, l)]++;
+	}
+	ll last = l, cnt = 0;
+	for(auto [i, v]: mp){
+		ll d = i-last;
+		last = i;
+		psa[cnt] += d;
+		cnt += v;
+		if(i == r+1) break;
+	}
+	cout<<psa[0]<<'\n';
+	for(int i = 1; i <= n; i++){
+		psa[i] += psa[i-1];
+		cout<<psa[i]<<'\n';
+	}
 }
